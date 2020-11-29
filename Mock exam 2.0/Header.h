e51a75d1e72ebb5c28df52d1d438ef8365f5e8e6
Meijkiqman <70415668+Meijkiqman@users.noreply.github.com>
2020-11-29 18:07:27 +0100

    update on it

--- Mock exam 2.0/Header.h
+++ Mock exam 2.0/Header.h
@@ -2,6 +2,11 @@
 void board();
 bool move(int attempts);
 void mainMenu();
+void task1();
+void task2();
+void task3();
+void task4();
+void task5();
 int moves = 4;
 
 
@@ -10,10 +15,15 @@ int Y = 3;
 int xpos = 0;
 int ypos = 0;
 
-
+int dice1;
+int dice2;
+int dice3;
+int dice4;
+int dice5;
+int dice6;
 
 
 std::vector<std::vector<int>>numbers{ { 1, 2, 3}, { 4, 5, 6}, {7, 8, 9 } };
 
-std::vector<char> password{ 's', 'd', 'w', 'w' };
+std::vector<char> password{ 's', 'd', 'w', 'd' };
 std::vector<char> myMoves{};
--- Mock exam 2.0/Mock exam 2.0.cpp
+++ Mock exam 2.0/Mock exam 2.0.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
 #include<conio.h>
+#include<time.h>
 #include "Header.h" 
+
 
 
 
@@ -58,8 +60,8 @@ bool move(int attempts)
 {
 	for (int i = 0; i < attempts; i++)
 	{
-		xpos == 0;
-		ypos == 0;
+		xpos = 0;
+		ypos = 0;
 		myMoves = {};
 		for (int k = 0; k < moves; k++)
 		{
@@ -110,12 +112,316 @@ bool move(int attempts)
 		}
 		if (myMoves == password)
 		{
+			std::cout << "\nSucces!\n";
+			system("pause");
 			return true;
 		}
+
 	}
 	return false;
 }
 void mainMenu()
 {
-	std::cout << "main menu";
+	system("cls");
+	std::cout << "Main Menu\n";
+	std::cout << "Welcome to the main menu please choose a task. \n";
+	std::vector<std::string> menu{ "task1", "task2", "task3", "task4", "task5" };
+	for (int i = 0; i < menu.size(); i++)
+	{
+		std::cout << i + 1 << ". " << menu[i] << std::endl;
+	}
+	char input;
+	input = _getch();
+	switch (input)
+	{
+	case '1':
+		task1();
+		break;
+	case '2':
+		task2();
+		break;
+	case '3':
+		task3();
+		break;
+	case '4':
+		task4();
+		break;
+	case '5':
+		task5();
+		break;
+	default:
+		std::cout << "please choose a valid option\n";
+		mainMenu();
+		break;
+	}
+}
+
+void task1()
+{
+	system("cls");
+	std::cout << "this tasks throws dice(1-6) 2000 times, it will count how many times it got each number and display them \n ";
+	std::cout << "press any key to throw the dice \n";
+	_getch();
+
+	for (int i = 0; i < 2000; i++)
+	{
+		int dice;
+
+		dice = rand() % 6 + 1;
+		if (dice == 1)
+		{
+			dice1 += 1;
+		}
+		else if (dice == 2)
+		{
+			dice2 += 1;
+		}
+		else if (dice == 3)
+		{
+			dice3 += 1;
+		}
+		else if (dice == 4)
+		{
+			dice4 += 1;
+		}
+		else if (dice == 5)
+		{
+			dice5 += 1;
+		}
+		else if (dice == 6)
+		{
+			dice6 += 1;
+		}
+
+	}
+	std::cout << "1: " << dice1 << " Times.\n\n";
+	std::cout << "2: " << dice2 << " Times.\n\n";
+	std::cout << "3: " << dice3 << " Times.\n\n";
+	std::cout << "4: " << dice4 << " Times.\n\n";
+	std::cout << "5: " << dice5 << " Times.\n\n";
+	std::cout << "6: " << dice6 << " Times.\n\n";
+
+}
+
+class Classvalue
+{
+	char name;
+	int value;
+	int place;
+};
+void task2()
+{
+
+}
+
+void task3()
+{
+	//declare grid:
+	const int Xaxis = 25;
+	const int Yaxis = 15;
+	int xpos = 1;
+	int ypos = 1;
+
+	char grid[Yaxis][Xaxis];
+
+	bool moving = true;
+
+	// fiil the grid with random "#"
+	for (int i = 0; i < Yaxis; i++)
+	{
+		for (int k = 0; k < Xaxis; k++) //fills border with "#"
+		{
+			if (i == 0 || i == Yaxis - 1)
+			{
+				grid[i][k] = '#';
+			}
+			else if (k == 0 || k == Xaxis - 1)
+			{
+				grid[i][k] = '#';
+			}
+			else if ((rand() % 20) < 2)//populates the grid with random "#"
+			{
+				grid[i][k] = '#';
+			}
+			else
+			{
+				grid[i][k] = ' ';
+			}
+
+		}
+	}
+	while (moving)
+	{
+		system("cls");
+		std::cout << "controls are WASD" << std::endl;
+		for (int i = 0; i < Yaxis; i++)
+		{
+			for (int k = 0; k < Xaxis; k++)
+			{
+				if (i == ypos && k == xpos) //player
+				{
+					std::cout << 'O';
+				}
+				else
+				{
+					std::cout << grid[i][k];
+				}
+			}
+			std::cout << std::endl;
+		}
+		char input;
+		input = _getch();
+		switch (input) //the if statements checks if theres and ' ' space to each side, if yes you can move.
+		{
+		case 'w':
+			if (grid[ypos - 1][xpos] == ' ')
+			{
+				ypos -= 1;
+			}
+
+			break;
+		case 's':
+			if (grid[ypos + 1][xpos] == ' ')
+			{
+				ypos += 1;
+			}
+			break;
+
+		case 'a':
+			if (grid[ypos][xpos - 1] == ' ')
+			{
+				xpos -= 1;
+			}
+			break;
+
+		case 'd':
+			if (grid[ypos][xpos + 1] == ' ')
+			{
+				xpos += 1;
+			}
+			break;
+		default:
+			std::cout << "invalid input";
+			break;
+
+		}
+		
+	}
+}
+
+void task4()
+{
+	int points = 0;
+
+	//declare grid
+
+	const int Xaxis = 25;
+	const int Yaxis = 15;
+	int xpos = 1;
+	int ypos = 1;
+
+	char grid[Yaxis][Xaxis];
+
+	bool moving = true;
+
+
+	//makes borders and wall out of '#'
+	for (int i = 0; i < Yaxis; i++)
+	{
+		for (int k = 0; k < Xaxis; k++) //fills border with "#"
+		{
+			if (i == 0 || i == Yaxis - 1)
+			{
+				grid[i][k] = '#';
+			}
+			else if (k == 0 || k == Xaxis - 1)
+			{
+				grid[i][k] = '#';
+			}
+			else if ((rand() % 20) < 2)//populates the grid with random "#"
+			{
+				grid[i][k] = '#';
+			}
+			else if ((rand() % 20) < 5)
+			{
+				grid[i][k] = '-'; // populates the grid with random points
+			}
+			else
+			{
+				grid[i][k] = ' ';
+			}
+
+		}
+	}
+	
+	while (moving)
+	{
+		system("cls");
+		std::cout << "controls are WASD" << std::endl;
+		for (int i = 0; i < Yaxis; i++)
+		{
+			for (int k = 0; k < Xaxis; k++)
+			{
+				if (i == ypos && k == xpos) //player
+				{
+					std::cout << 'O';
+				}
+				else
+				{
+					std::cout << grid[i][k];
+				}
+			}
+			std::cout << std::endl;
+		}
+		std::cout << "\npoints: " << points;
+		char input;
+		input = _getch();
+		switch (input) //the if statements checks if theres a '#', if it is it cant move there.
+		case 'w':
+		{
+			if (grid[ypos - 1][xpos] != '#')
+			{
+				ypos -= 1;
+			}
+
+			break;
+		case 's':
+			if (grid[ypos + 1][xpos] != '#')
+			{
+				ypos += 1;
+			}
+			break;
+
+		case 'a':
+			if (grid[ypos][xpos - 1] != '#')
+			{
+				xpos -= 1;
+			}
+			break;
+
+		case 'd':
+			if (grid[ypos][xpos + 1] != '#')
+			{
+				xpos += 1;
+			}
+			break;
+		default:
+			std::cout << "invalid input";
+			break;
+
+		}
+		//checks if the player/"O" is on a "-" then deletets the "-"
+		if (grid[ypos][xpos] == '-')
+		{
+			points += 1;
+			grid[ypos][xpos] = ' '; //replaces "-" with " " when player is on said grid
+		}
+
+	}
+	
+}
+
+void task5()
+{
+	std::cout << "goodbye" << std::exit;
 }
